57f6a6718c417f62e347b861a920fa23f89db63b
nachoNota <nachonota13@gmail.com>
2024-06-02 19:47:18 -0300

    g

--- Operacion.cs
+++ Operacion.cs
@@ -5,36 +5,8 @@ public class Operacion{
     private double nuevoValor;
     private TipoOperacion operacion;
 
-    public Operacion(){
-        resultadoAnterior = 0;
-        nuevoValor = 0;
-        operacion = TipoOperacion.Limpiar;
-    }
+    public double NuevoValor { get { return nuevoValor; } set { nuevoValor = value; } }
+
+
     
-}
-
-public class Calculadora {
-
-
-    private double dato;
-    public double Resultado { get => dato;}
-    public void Sumar(double termino){
-        dato += termino;
-    }
-
-    public void Restar(double termino){
-        dato -= termino;
-    }
-
-    public void Multiplicar(double termino){
-        dato *= termino;
-    }
-
-    public void Dividir(double termino){
-        dato /= termino;
-    }
-
-    public void Limpiar(){
-        dato = 0;
-    } 
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿
 
+Console.WriteLine("----MENU----");
+Console.WriteLine("1- Suma");
+Console.WriteLine("2- Resta");
+Console.WriteLine("3- Multiplicacion");
+Console.WriteLine("4- Division");
+Console.WriteLine("Opcion: ");
