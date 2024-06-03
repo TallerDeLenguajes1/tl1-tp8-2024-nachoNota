b8cfecb5a35063f6baaa765c8c2e5c0c2623d642
nachoNota <nachonota13@gmail.com>
2024-06-03 11:41:43 -0300

    calculadora completada

--- Operacion.cs
+++ Operacion.cs
@@ -1,12 +1,52 @@
 namespace CalculadoraHistorial;
 
-public class Operacion{
+public class Operacion
+{
     private double resultadoAnterior;
     private double nuevoValor;
     private TipoOperacion operacion;
 
-    public double NuevoValor { get { return nuevoValor; } set { nuevoValor = value; } }
+    public double Resultado { get { return nuevoValor; } }
+    public double ResultadoAnterior { get {  return resultadoAnterior; } set { resultadoAnterior = value; } }
+    public TipoOperacion Operation { get { return operacion; } set { operacion = value; } }
 
+    public Operacion(double resultadoAntrerior)
+    {
+        this.resultadoAnterior = resultadoAntrerior;
+        this.nuevoValor = resultadoAntrerior;
+    }
 
+    public void Suma(double valor)
+    {
+        resultadoAnterior = valor;
+        nuevoValor += valor;
+        operacion = TipoOperacion.Suma;
+    }
+
+    public void Resta(double valor)
+    {
+        resultadoAnterior = valor;
+        nuevoValor -= valor;
+        operacion = TipoOperacion.Resta;
+    }
     
+    public void Multiplicar(double valor)
+    {
+        resultadoAnterior = valor;
+        nuevoValor *= valor;
+        operacion = TipoOperacion.Multiplicacion;
+    }
+
+    public void Dividir(double valor)
+    {
+        resultadoAnterior = valor;
+        nuevoValor /= valor;
+        operacion = TipoOperacion.Division;
+    }
+
+    public void Limpiar()
+    {
+        nuevoValor = 0;
+        operacion = TipoOperacion.Limpiar;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,88 @@
 ﻿
+using CalculadoraHistorial;
 
-Console.WriteLine("----MENU----");
-Console.WriteLine("1- Suma");
-Console.WriteLine("2- Resta");
-Console.WriteLine("3- Multiplicacion");
-Console.WriteLine("4- Division");
-Console.WriteLine("Opcion: ");
+var Historial = new List<Operacion>();
+double num = 0, actual = 0;
+int opcion = 0;
+
+do
+{
+    Console.WriteLine("----MENU----");
+    Console.WriteLine("1- Suma");
+    Console.WriteLine("2- Resta");
+    Console.WriteLine("3- Multiplicacion");
+    Console.WriteLine("4- Division");
+    Console.WriteLine("5- Limpiar");
+    Console.WriteLine("6- Mostrar historial");
+    Console.WriteLine("7- Salir");
+    Console.WriteLine("Opcion: ");
+    opcion = int.Parse(Console.ReadLine());
+
+    if (opcion != 5 && opcion != 6 && opcion != 7)
+    {
+        Console.WriteLine("Ingrese numero: ");
+        num = double.Parse(Console.ReadLine());
+    }
+
+    var NuevaOperacion = new Operacion(actual);
+
+    if(opcion != 7) { 
+        switch (opcion)
+        {
+            case 1:
+                NuevaOperacion.Suma(num);
+                actual = NuevaOperacion.Resultado;
+                break;
+
+            case 2:
+                NuevaOperacion.Resta(num);
+                actual = NuevaOperacion.Resultado;
+                break;
+
+            case 3:
+                NuevaOperacion.Multiplicar(num);
+                actual = NuevaOperacion.Resultado;
+                break;
+
+            case 4:
+                if (num == 0)
+                {
+                    Console.WriteLine("No puede realizar la operacion");
+                }
+                else
+                {
+                    NuevaOperacion.Dividir(num);
+                    actual = NuevaOperacion.Resultado;
+                }
+                break;
+
+            case 5:
+                NuevaOperacion.Limpiar();
+                actual = 0;
+                break;
+
+            case 6:
+                int i = 0;
+                foreach (var oper in Historial)
+                {
+                    Console.WriteLine($"Iteracion: {i}");
+                    Console.WriteLine($"Numero ingresado: {oper.ResultadoAnterior}");
+                    Console.WriteLine($"Operacion realizada: {oper.Operation}");
+                    Console.WriteLine($"Resultado obtenido: {oper.Resultado}");
+                    Console.WriteLine("=====================================");
+                    i++;
+                }
+                break;
+
+            default:
+                Console.WriteLine("La opcion ingresada no es valida");
+                break;
+        }
+    }
+
+    if (opcion != 6)
+    {
+        Historial.Add(NuevaOperacion);
+    }
+
+} while (opcion != 7);
